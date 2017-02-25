bc7d5b58bdc463fd82d38ed58496b896ba82c5ab
hsmallbone <21306592@student.uwa.edu.au>
2017-02-25 15:11:23 +0800

    Use Unifi JSON API for radius sync instead of MySQL

--- wifi-credential-sync/sync.py
+++ wifi-credential-sync/sync.py
@@ -1,29 +1,26 @@
 import datetime
 import os
 import logging
+import json
 
 # Fix for SNI problems on older Python versions
 import urllib3.contrib.pyopenssl
 urllib3.contrib.pyopenssl.inject_into_urllib3()
 
-from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
-from models import *
+from unifi.controller import Controller
 
 import requests
 from requests.auth import HTTPBasicAuth
 
 from config import *
+c = Controller(CONTROLLER_IP, CONTROLLER_USERNAME, CONTROLLER_PASSWORD, CONTROLLER_PORT, CONTROLLER_VERSION, CONTROLLER_SITEID)
 
 logging.basicConfig(format='[%(asctime)s] %(levelname)s: %(message)s', level=logging.INFO)
 logging.info('Starting sync')
 
-logging.debug('Connecting to database')
-e = create_engine("mysql://{}:{}@{}/{}".format(DB_USER, DB_PASS, DB_HOST, DB_NAME))
-e.connect()
-
-logging.debug('Constructing session')
-s = Session(bind=e)
+logging.debug('Fetching users')
+users = [[x['_id'], x['name'], x['x_password']] for x in c.get_radius_users()]
+user_name_mapping = {}
 
 logging.debug('Querying Bloom Memberships API')
 r = requests.get('https://{}/api/users'.format(HOST), headers={'Authorization': 'Token {}'.format(TOKEN)})
@@ -31,34 +28,29 @@ r = requests.get('https://{}/api/users'.format(HOST), headers={'Authorization': 
 if r.status_code != 200:
 	logging.error('API query caused status code {} instead of expected 200'.format(r.status_code))
 
-
 counters = {v : 0 for v in ['updated', 'skipped_unchanged', 'skipped_inactive', 'deleted', 'created']}
 
 logging.debug('Beginning iteration')
 for data in r.json()['users']:
 	username = data['wifi_username']
 
-	if s.query(UserInfo).filter(UserInfo.username == username, UserInfo.creationby != 'memberships-sync').count():
-		logging.warning('Skipped {}, as existing unmanaged user with same name'.format(username))
-		continue
-
-	u = s.query(UserInfo).filter_by(username=username, creationby='memberships-sync').first()
+	u = None
+	for user in users:
+		if user[1] == username:
+			u = user 
+			break
 
 	# If pre-existing user who is enabled, we update their details
 	if u and data['wifi_access?']:
-		pw = s.query(RADCheck).filter_by(username=username, attribute='Cleartext-Password').first()
-
+		users.remove(u)
+		pw = u[2]
+		user_name_mapping[u[0]] = {'firstname': data['firstname'], 'lastname': data['lastname']}
 		# If data has actually changed, we update the database
-		if u.firstname != data['firstname'] or u.lastname != data['lastname'] or pw.value != data['wifi_password']:
-			u.firstname = data['firstname']
-			u.lastname = data['lastname']
-
-			u.updatedate = datetime.datetime.now()
-			u.updateby = 'memberships-sync'
-
-			pw.value = data['wifi_password']
-
-			s.commit()
+		if pw != data['wifi_password']:
+			try:
+				c.put_radius_user(u[0], u[1], data['wifi_password'])
+			except:
+				logging.error('API query error updating wifi password of {} {}'.format(u[1], u[0]))
 
 			logging.info('Updated {}'.format(username))
 			counters['updated'] += 1
@@ -68,33 +60,22 @@ for data in r.json()['users']:
 
 	# If pre-existing user who is disabled, we delete their UserInfo and password
 	elif u and not data['wifi_access?']:
-		for rc in u.radchecks:
-			s.delete(rc)
-		s.delete(u)
+		users.remove(u)
+		try:
+			c.delete_radius_user(u[0])
+		except:
+			logging.error('API error deleting radius user {}'.format(u[0]))
 
 		logging.info('Deleted {}'.format(username))
 		counters['deleted'] += 1
 
 	# If new user who is enabled and has set their wifi password, we create their account
 	elif not u and data['wifi_access?'] and data['wifi_password']:
-		u = UserInfo()
-		u.username = data['wifi_username']
-		u.firstname = data['firstname']
-		u.lastname = data['lastname']
-		u.email = data['wifi_username']
-
-		u.creationdate = datetime.datetime.now()
-		u.creationby = 'memberships-sync'
-
-		rc = RADCheck()
-		rc.user = u
-		rc.attribute = 'Cleartext-Password'
-		rc.op = ':='
-		rc.value = data['wifi_password']
-
-		s.add(u)
-		s.add(rc)
-		s.commit()
+		try:
+			c.add_radius_user(data['wifi_username'], data['wifi_password'])
+		except:
+			logging.error('API error adding radius user {}'.format(data['wifi_username']))
+		user_name_mapping[data['wifi_username']] = {'firstname': data['firstname'], 'lastname': data['lastname']}
 
 		logging.info('Created {}'.format(username))
 		counters['created'] += 1
@@ -103,5 +84,16 @@ for data in r.json()['users']:
 		logging.debug('Skipped {} as is inactive/disabled'.format(username))
 		counters['skipped_inactive'] += 1
 
+for user in users:
+	if user[0]:
+		try:
+			c.delete_radius_user(user[0])
+			logging.info('Deleted {}'.format(user[1]))
+			counters['deleted'] += 1
+		except:
+			logging.error('Error deleting outdated wifi user {}'.format(user[1]))
+
+with open('user_mapping.json', 'w') as out:
+	out.write(json.dumps(user_name_mapping))
 logging.info('Stats: ' + ', '.join('{}: {}'.format(t, c) for t, c in counters.items()))
 logging.info('Sync completed')
